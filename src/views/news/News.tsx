4c032a01ccd72f764195b5a520d450f2538b46e7
Leejjon <leejjon@gmail.com>
2019-11-18 22:06:35 +0100

    Filled the dummy pages.

---
+++ src/views/news/News.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const News: React.FC = () => {
+    return (
+        <div>
+            <h1>News page</h1>
+            <p>This is the news page</p>
+        </div>
+    );
+};
+
+export default News;
