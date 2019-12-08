736fd10a0db116ca20f75f098013792f8b149fa6
Leejjon <leejjon@gmail.com>
2019-12-09 00:37:48 +0100

    Kind of updated the minimal test.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,12 +5,13 @@ import "@testing-library/jest-dom/extend-expect";
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const div = document.createElement('div');
+    ReactDOM.render(<App/>, div);
+    ReactDOM.unmountComponentAtNode(div);
 });
 
-test('renders welcome message', () => {
-  const {getByText} = render(<App/>);
-  expect(getByText('Learn React')).toBeInTheDocument();
+test('renders links', () => {
+    const {container, getByText} = render(<App/>);
+
+    expect(getByText("Home page")).toBeInTheDocument();
 });
