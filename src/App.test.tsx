0a18d660d6c340fc9099f5686d4fee3d2fefa456
Leejjon <leejjon@gmail.com>
2019-12-19 01:03:19 +0100

    Introduced casting.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -10,52 +10,40 @@ test('Renders without crashing', async() => {
 });
 
 test('Verify home page header', async() => {
-    let app: HTMLElement;
+    let app: HTMLElement | undefined = undefined;
     await act(async () => {
         const {container} = render(<App/>);
         app = container;
     });
-    // @ts-ignore
-    if (app) {
-        const pageHeaderContent = app.querySelector("#pageHeader")?.firstChild?.textContent;
-        expect(pageHeaderContent).toMatch('Home page');
-    } else {
-        fail("The app should have been initialized.");
-    }
+    let appAsHtmlElement = (app as unknown as HTMLElement);
+    const pageHeaderContent = appAsHtmlElement.querySelector("#pageHeader")?.firstChild?.textContent;
+    expect(pageHeaderContent).toMatch('Home page');
 });
 
 test('Navigate to news', async () => {
-    let app: HTMLElement;
+    let app: HTMLElement | undefined = undefined;
     await act(async () => {
         const {container} = render(<App/>);
         app = container;
     });
 
-    // @ts-ignore
-    if (app) {
-        const linkToNewsElement: Element = (app.querySelector('#linkToNews') as Element);
-        fireEvent.click(linkToNewsElement);
-        const pageHeaderContent = await waitForElement(() => app.querySelector('#pageHeader')?.firstChild?.textContent);
-        expect(pageHeaderContent).toMatch('News page');
-    } else {
-        fail("The app should have been initialized.");
-    }
+    let appAsHtmlElement = (app as unknown as HTMLElement);
+    const linkToNewsElement: Element = (appAsHtmlElement.querySelector('#linkToNews') as Element);
+    fireEvent.click(linkToNewsElement);
+    const pageHeaderContent = await waitForElement(() => appAsHtmlElement.querySelector('#pageHeader')?.firstChild?.textContent);
+    expect(pageHeaderContent).toMatch('News page');
 });
 
 test('Navigate to about', async () => {
-    let app: HTMLElement;
+    let app: HTMLElement | undefined = undefined;
     await act(async () => {
         const {container} = render(<App/>);
         app = container;
     });
 
-    // @ts-ignore
-    if (app) {
-        const linkToAboutElement: Element = (app.querySelector('#linkToAbout') as Element);
-        fireEvent.click(linkToAboutElement);
-        const pageHeaderContent = await waitForElement(() => app.querySelector('#pageHeader')?.firstChild?.textContent);
-        expect(pageHeaderContent).toMatch('About page');
-    } else {
-        fail("The app should have been initialized.");
-    }
+    let appAsHtmlElement = (app as unknown as HTMLElement);
+    const linkToAboutElement: Element = (appAsHtmlElement.querySelector('#linkToAbout') as Element);
+    fireEvent.click(linkToAboutElement);
+    const pageHeaderContent = await waitForElement(() => appAsHtmlElement.querySelector('#pageHeader')?.firstChild?.textContent);
+    expect(pageHeaderContent).toMatch('About page');
 });
